edd2de738aa09aa0364222de4185fc51eba33f05
Jair Deyner <jairdeyner730791@gmail.com>
2023-03-28 18:58:14 -0500

    Finish

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'products-numbers-page',
   templateUrl: './numbers-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NumbersPageComponent {
-
+  public totalSells: number = 256786.5567;
+  public percent: number = 0.4856;
 }
